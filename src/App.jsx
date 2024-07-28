bdd7da94c41ab6dd5396cc8ba27c29bcb69e8df4
Allan Juma <allancjuma@gmail.com>
2024-07-28 15:28:56 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+export default function App() {
+  return (
+    <main className="w-screen">
+      <nav className="w-full flex flex-row">
+        <div className="">LOGO</div>
+        <ul className="">
+          <li className="">Home</li>
+          <li>About</li>
+          <li>Book Now</li>
+        </ul>
+      </nav>
+    </main>
+  );
+}
