ee826fdc27285f87ad5313e2de03082d8051fec7
Allan Juma <allancjuma@gmail.com>
2024-07-28 15:49:15 +0300

    add:bg image and nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
+import "./App.css";
+
 export default function App() {
   return (
-    <main className="w-screen">
-      <nav className="w-full flex flex-row">
-        <div className="">LOGO</div>
-        <ul className="">
-          <li className="">Home</li>
-          <li>About</li>
-          <li>Book Now</li>
-        </ul>
-      </nav>
+    <main className="w-screen h-screen text-white">
+      <div id="nav-hero" className="px-24 pt-5">
+        <nav className="w-full flex justify-between static">
+          <div className="">LOGO</div>
+          <ul className="flex gap-8">
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Book Now</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </main>
   );
 }
