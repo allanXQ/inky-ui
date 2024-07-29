32b60668c48bbd14737bdb8e890176e8360c9675
Allan Juma <allancjuma@gmail.com>
2024-07-29 22:04:49 +0300

    add quote

--- src/App.jsx
+++ src/App.jsx
@@ -156,6 +156,50 @@ export default function App() {
         </div>
         <img src="./inky2.webp" />
       </div>
+      <div
+        id="quote"
+        className="flex flex-col items-center justify-center w-full relative"
+      >
+        <div>
+          <div
+            id="comp-ky2hvz0a"
+            class="comp-ky2hvz0a wixui-vector-image"
+            data-motion-enter="done"
+          >
+            <div
+              data-testid="svgRoot-comp-ky2hvz0a"
+              class="AKxYR5 VZYmYf comp-ky2hvz0a"
+            >
+              <svg
+                preserveAspectRatio="xMidYMid meet"
+                data-bbox="0 0 770.35 1260.4"
+                viewBox="0 0 770.35 1260.4"
+                xmlns="http://www.w3.org/2000/svg"
+                data-type="color"
+                role="presentation"
+                aria-hidden="true"
+              >
+                <g>
+                  <path
+                    d="M456 210.92q10.37-54.67 19.16-75C306.82 265.76 140.68 430.42 195.6 659.86Q158.89 602.47 161 498q-35.15 121.74-32.85 301.22C113 754.81 102.81 651.36 111.3 590.77 26.78 692.34-17.87 931.93 80.94 1010.4 70.76 1036.21 27.58 1167.58 0 1259.84c0 0 .48.21 1.4.56l51.27-42c94.09-377.35 281.19-808.2 547.89-1093.68q-304.69 430.2-439.83 874.42c269.69-95.91 209.45-395.57 349.6-523.35-40.48 7-78.95 42.61-102.46 70.28C446.65 453.81 551.81 462.59 592 296.2 615.42 199.05 694.48 80.87 770.35 0Q512.27 62 456 210.92z"
+                    fill="#f6c228"
+                    data-color="1"
+                  ></path>
+                </g>
+              </svg>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-2 items-center w-[600px] border border-white rounded-lg p-10 ">
+          <p>
+            The one thing we all have in common is that we will encounter
+            adversity, but we have to decide how we respond to it. My arm and my
+            hand are paralyzed but my heart isn’t, my mind isn’t, my dedication
+            isn’t, my work ethic isn’t, my commitment isn’t.”
+          </p>
+          <p>-INKY</p>
+        </div>
+      </div>
     </main>
   );
 }
