ea76bfa0e63c80c392d305f40a7e9f712d3288fa
Allan Juma <allancjuma@gmail.com>
2024-08-02 23:55:46 +0300

    update layout

--- src/Layouts/index.jsx
+++ src/Layouts/index.jsx
@@ -2,8 +2,8 @@ import React, { Children } from "react";
 
 const Layout = ({ children }) => {
   return (
-    <div className="w-screen">
-      <nav className=" flex items-center justify-between px-24  text-white">
+    <div className="w-screen flex flex-col items-center justify-center">
+      <nav className="flex items-center justify-between text-white w-screen px-10">
         <div className="">
           <img src="./inkylogo.png" width={100} height={100} alt="Inky Logo" />
         </div>
@@ -20,7 +20,7 @@ const Layout = ({ children }) => {
         </ul>
       </nav>
       {children}
-      <footer className="flex flex-col items-center justify-center gap-2 mt-10">
+      <footer className="flex flex-col items-center justify-center gap-2 mt-10 w-screen">
         <img src="./footerlogo.png" />
         <div className="flex items-center justify-center gap-2">
           <img src="./ig.webp" />
@@ -29,7 +29,7 @@ const Layout = ({ children }) => {
           <img src="./linkedin.webp" />
           <img src="./yt.webp" />
         </div>
-        <p className="text-white">
+        <p className="text-white text-center">
           © 2023 by Inky Johnson. All Rights Reserved.
         </p>
       </footer>
