b137108f596a0eb1fdb0c59d9705ac2285a635cf
Allan Juma <allancjuma@gmail.com>
2024-08-03 00:30:47 +0300

    update content

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,8 +2,45 @@ import React from "react";
 
 const About = () => {
   return (
-    <main id="about" className="w-screen">
+    <main id="about" className="w-screen ">
       <div id="about-hero"></div>
+
+      <div className="flex flex-col justify-center items-center py-10">
+        <div className="w-[70rem] flex flex-col gap-5">
+          <p className="font-bold">
+            Husband, father, collegiate athlete, entrepreneur and author, Inky
+            Johnson is one of the most highly sought after speakers in the
+            world. For over a decade, executives, professional sports teams,
+            business owners and people all over have benefited from the raw
+            energy of his thought provoking and inspirational presentations.
+            Whether the topic is leadership, teamwork, excelling in the midst of
+            adversity, embracing change, mental agility or perseverance, Inky’s
+            message is effective and efficient.
+          </p>
+          <p>
+            Inky’s gift of being an effective communicator has allowed him to
+            crossover into different industries. He is a leader called upon by
+            leaders. A partial list of his clients include Chick-fil-A, Dell
+            Technologies, Aflac Insurance, Cooks Pest Control, State Farm, AT&T
+            Comcast, Coca Cola, Bank of America, Stryker, The University Of
+            Alabama, The University of Texas, The University of Oregon. UCLA,
+            Clemson University, Orlando Magic, New York Knicks, Minnesota
+            Timberwolves, The New England Patriots, Tampa Bay Buccaneers, Los
+            Angeles Rams, New York Knicks, Baltimore Ravens, Chicago Cubs and
+            Atlanta Braves.
+          </p>
+          <p>
+            Inky earned his Master’s Degree in Sports Psychology from The
+            University of Tennessee. He was a three year letterman and two year
+            Captain on the Football team, where he started as a cornerback. In
+            his junior year he was projected as a top thirty draft pick.
+            Unfortunately, on September 9th, 2006, he sustained a life
+            threatening, career ending injury that paralyzed his right arm and
+            hand. From that day to present day Inky has had a burning desire to
+            use his situation and experiences to add value to people’s lives.
+          </p>
+        </div>
+      </div>
     </main>
   );
 };
