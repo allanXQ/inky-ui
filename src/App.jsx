88dab2222fbe5054969b69403992b19e0c5a8cb8
Allan Juma <allancjuma@gmail.com>
2024-08-02 23:46:43 +0300

    add: layout, transparent logo

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import { Route, Routes } from "react-router";
 import "./App.css";
 import Home from "./Pages/Home";
+import Layout from "./Layouts";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+      </Route>
     </Routes>
   );
 };
--- src/Layouts/index.jsx
+++ src/Layouts/index.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { Children } from "react";
 
-const Layout = () => {
-  return <div>Layout</div>;
+const Layout = ({ children }) => {
+  return (
+    <div className="w-screen">
+      <nav className=" flex items-center justify-between px-24  text-white">
+        <div className="">
+          <img src="./inkylogo.png" width={100} height={100} alt="Inky Logo" />
+        </div>
+        <ul className="flex items-center gap-8">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li className="bg-[#F6C228] text-black font-bold py-1 px-3 rounded-lg">
+            <a href="#">Book Now</a>
+          </li>
+        </ul>
+      </nav>
+      {children}
+      <footer className="flex flex-col items-center justify-center gap-2 mt-10">
+        <img src="./footerlogo.png" />
+        <div className="flex items-center justify-center gap-2">
+          <img src="./ig.webp" />
+          <img src="./fb.webp" />
+          <img src="./x.webp" />
+          <img src="./linkedin.webp" />
+          <img src="./yt.webp" />
+        </div>
+        <p className="text-white">
+          © 2023 by Inky Johnson. All Rights Reserved.
+        </p>
+      </footer>
+    </div>
+  );
 };
 
 export default Layout;
