8bf7458cae50ab48aded4c99238518124637d57c
Allan Juma <allancjuma@gmail.com>
2024-08-03 23:27:00 +0300

    update styles

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -341,9 +341,37 @@ export default function Home() {
       >
         <div className="flex gap-4">
           <img src="./lines.png" />
-          <div className="flex flex-col gap-4 items-center justify-center">
-            <p>BOOK MD TODAY!</p>
-            <ButtonArrow text="Book Now" />
+          <div className="flex flex-col gap-4  justify-center">
+            <div className="flex flex-col font-bold gap-2 text-5xl">
+              <div className="flex  gap-2">
+                <p>BOOK</p> <p className="text-[#F6C228]">MD</p>
+              </div>
+              <p>TODAY!</p>
+            </div>
+            <div className="bg-[#F6C228] text-black flex items-center rounded-lg w-36 ">
+              <button
+                id="hero-book-btn"
+                className="py-2 px-4 border-r border-black"
+                onClick={() => navigate("/book")}
+              >
+                <p className="text-md">Book Now</p>
+              </button>
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-8 pl-3"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+                />
+              </svg>
+            </div>
           </div>
         </div>
         <div className="flex flex-col gap-4 border border-[#636363] max-w-[900px] p-10 relative">
