4b5ebf6ce1382b71589e4b2e00faf1919ef2f158
Allan Juma <allancjuma@gmail.com>
2024-08-03 13:19:20 +0300

    add form fields

--- src/Pages/Book.jsx
+++ src/Pages/Book.jsx
@@ -1,52 +1,301 @@
 import React, { useState } from "react";
 
-const TextField = ({ label, id, width }) => {
+const inputs = [
+  {
+    title: "About You",
+    fields: [
+      { label: "First Name", id: "FirstName", type: "text" },
+      { label: "Last Name", id: "LastName", type: "text" },
+      { label: "Position", id: "Position", type: "text" },
+      { label: "Email", id: "Email", type: "email" },
+    ],
+  },
+  {
+    title: "Company Information",
+    fields: [
+      { label: "Company Name", id: "CompanyName", type: "text" },
+      { label: "Company Phone", id: "CompanyPhone", type: "tel" },
+      { label: "Company Website", id: "CompanyWebsite", type: "text" },
+      {
+        label: "Organization Type",
+        id: "OrganizationType",
+        type: "select",
+        options: [
+          "Religious Organisation",
+          "Small and Medium Enterprises(SMEs)",
+          "Corporate",
+          "Educational Institution",
+          "Other",
+        ],
+      },
+      {
+        label: "Company Address",
+        id: "CompanyAddress",
+        type: "textarea",
+        width: "full",
+      },
+    ],
+  },
+  {
+    title: "Event Details",
+    fields: [
+      {
+        label: "Event Name",
+        id: "EventName",
+        type: "text",
+      },
+      {
+        label: "Event Topic",
+        id: "EventTopic",
+        type: "text",
+      },
+      {
+        label: "Event Date(s) of Interest",
+        id: "EventDate",
+        type: "date",
+      },
+      {
+        label: "Event Budget for Speaker",
+        id: "EventBudget",
+        type: "text",
+      },
+      {
+        label: "Venue Name and Address",
+        id: "VenueNameAddress",
+        type: "text",
+        width: "full",
+      },
+      {
+        label: "Closest Airport",
+        id: "ClosestAirport",
+        type: "text",
+        width: "full",
+      },
+      {
+        label: "Will tickets be sold for this event?",
+        id: "TicketsSold",
+        type: "radio",
+        options: ["Yes", "No"],
+      },
+      {
+        label: "Planned Number of Attendees",
+        id: "Attendees",
+        type: "text",
+      },
+      {
+        label: "Will this event be open to the public?",
+        id: "OpenToPublic",
+        type: "radio",
+        options: ["Yes", "No"],
+      },
+      {
+        label: "Will you record this event?",
+        id: "Recorded",
+        type: "radio",
+        options: ["Yes", "No"],
+      },
+      {
+        label: "What are you booking MD for?",
+        id: "BookingFor",
+        type: "select",
+        options: [
+          "Corporate Training",
+          "Live and Virtual Keynotes",
+          "Breakout Sessions",
+          "Corporate Emcee",
+          "Professional Development Consulting",
+        ],
+      },
+    ],
+  },
+  {
+    title: "Presentation/Speech Details",
+    fields: [
+      {
+        label: "Event Start Time",
+        id: "EventStartTime",
+        type: "time",
+      },
+      {
+        label: "Speaking Time",
+        id: "SpeakingTime",
+        type: "text",
+        placeholder: "Include the time MD would speak",
+      },
+      {
+        label: "Expected Duration",
+        id: "ExpectedDuration",
+        type: "text",
+        placeholder: "30 min, 1 hour, etc",
+      },
+      {
+        label: "Email",
+        id: "Email",
+        type: "email",
+      },
+      {
+        label: "Additional Information",
+        id: "AdditionalInformation",
+        type: "textarea",
+        notrequired: true,
+      },
+    ],
+  },
+];
+
+const TextField = ({ label, id, type, width, placeholder }) => {
   return (
-    <div className="flex flex-col gap-2">
+    <div className={`flex flex-col gap-2 ${width === "full" ? "w-full" : ""}`}>
       <label htmlFor={id}>{label}</label>
-      <input type="text" id={id} className="w-full" />
+      <input
+        type={type}
+        id={id}
+        name={id}
+        placeholder={placeholder}
+        className="border border-gray-300 rounded-md p-2"
+      />
     </div>
   );
 };
 
+const TextArea = ({ label, id, type, width, placeholder }) => {
+  return (
+    <div className={`flex flex-col gap-2 ${width === "full" ? "w-full" : ""}`}>
+      <label htmlFor={id}>{label}</label>
+      <textarea
+        id={id}
+        name={id}
+        placeholder={placeholder}
+        className="border border-gray-300 rounded-md p-2"
+      />
+    </div>
+  );
+};
+
+const SelectField = ({ label, id, type, options }) => {
+  return (
+    <div className="flex flex-col gap-2">
+      <label htmlFor={id}>{label}</label>
+      <select
+        id={id}
+        name={id}
+        className="border border-gray-300 rounded-md p-2"
+      >
+        {Array.isArray(options) &&
+          options.map((option) => (
+            <option value={option} key={option}>
+              {option}
+            </option>
+          ))}
+      </select>
+    </div>
+  );
+};
+
+const RadioField = ({ label, id, type, options }) => {
+  return (
+    <div className="flex flex-col gap-2">
+      <label htmlFor={id}>{label}</label>
+      <div className="flex gap-2">
+        {Array.isArray(options) &&
+          options.map((option) => (
+            <div className="flex items-center gap-2" key={option}>
+              <input type="radio" id={option} name={id} value={option} />
+              <label htmlFor={option}>{option}</label>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+const TimeField = ({ label, id, type }) => {
+  return (
+    <div className="flex flex-col gap-2">
+      <label htmlFor={id}>{label}</label>
+      <input
+        type={type}
+        id={id}
+        name={id}
+        className="border border-gray-300 rounded-md p-2"
+      />
+    </div>
+  );
+};
+
+const BookForm = () => {
+  return (
+    <form className="flex flex-col gap-10">
+      {inputs.map((input) => (
+        <div className="flex flex-col gap-5" key={input.title}>
+          <h2>{input.title}</h2>
+          {input.fields.map((field) => {
+            switch (field.type) {
+              case "text":
+              case "email":
+              case "tel":
+                return (
+                  <TextField
+                    label={field.label}
+                    id={field.id}
+                    type={field.type}
+                    width={field.width}
+                    placeholder={field.placeholder}
+                    key={field.id}
+                  />
+                );
+              case "textarea":
+                return (
+                  <TextArea
+                    label={field.label}
+                    id={field.id}
+                    type={field.type}
+                    width={field.width}
+                    placeholder={field.placeholder}
+                    key={field.id}
+                  />
+                );
+              case "select":
+                return (
+                  <SelectField
+                    label={field.label}
+                    id={field.id}
+                    type={field.type}
+                    options={field.options}
+                    key={field.id}
+                  />
+                );
+              case "radio":
+                return (
+                  <RadioField
+                    label={field.label}
+                    id={field.id}
+                    type={field.type}
+                    options={field.options}
+                    key={field.id}
+                  />
+                );
+              case "time":
+                return (
+                  <TimeField
+                    label={field.label}
+                    id={field.id}
+                    type={field.type}
+                    key={field.id}
+                  />
+                );
+              default:
+                return null;
+            }
+          })}
+        </div>
+      ))}
+      <button className="bg-blue-500 text-white p-2 rounded-md">Submit</button>
+    </form>
+  );
+};
+
 const Book = () => {
-  const [formData, setFormData] = useState({
-    firstName: "",
-    lastName: "",
-    position: "",
-    email: "",
-  });
-  const [errors, setErrors] = useState({});
-
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-    // Clear errors
-    if (errors[e.target.name]) {
-      setErrors({
-        ...errors,
-        [e.target.name]: null,
-      });
-    }
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    let currentErrors = {};
-    // Simple validation: checks if the field is empty
-    Object.keys(formData).forEach((field) => {
-      if (!formData[field]) {
-        currentErrors[field] = "This field is required";
-      }
-    });
-    setErrors(currentErrors);
-    if (Object.keys(currentErrors).length === 0) {
-      console.log("Form data:", formData);
-      // Submit form logic here
-    }
-  };
   return (
     <main id="book" className="w-screen ">
       <div id="book-hero">
@@ -54,40 +303,7 @@ const Book = () => {
       </div>
 
       <div className="flex flex-col justify-center items-center gap-10 pt-10">
-        <form onSubmit={handleSubmit} noValidate>
-          <div className="space-y-4">
-            {Object.entries(formData).map(([key, value]) => (
-              <div key={key}>
-                <label
-                  htmlFor={key}
-                  className="block text-sm font-medium text-gray-700 capitalize"
-                >
-                  {key.replace(/([A-Z])/g, " $1")} *
-                </label>
-                <input
-                  type="text"
-                  name={key}
-                  id={key}
-                  value={value}
-                  onChange={handleChange}
-                  className={`mt-1 block w-full px-3 py-2 border ${
-                    errors[key] ? "border-red-500" : "border-gray-300"
-                  } rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`}
-                  placeholder={`Enter ${key}`}
-                />
-                {errors[key] && (
-                  <p className="mt-2 text-sm text-red-600">{errors[key]}</p>
-                )}
-              </div>
-            ))}
-          </div>
-          <button
-            type="submit"
-            className="mt-6 px-4 py-2 bg-blue-500 text-white rounded-md"
-          >
-            Submit
-          </button>
-        </form>
+        <BookForm />
       </div>
     </main>
   );
