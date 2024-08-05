48f392cc872ea0ddbd4d4f1501d65b61ba8fdcb2
Allan Juma <allancjuma@gmail.com>
2024-08-05 13:59:59 +0300

    update styles

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -3,7 +3,20 @@ import React from "react";
 const About = () => {
   return (
     <main id="about" className=" flex flex-col gap-5">
-      <div id="about-hero"></div>
+      <div
+        id="about-hero"
+        className="flex flex-col justify-center text-white gap-2 pl-5  md:pl-52 pt-10"
+      >
+        <div className="flex  gap-2 text-5xl text-wrap font-extrabold">
+          <p className="text-[#F6C228]">WHO IS</p>
+          <p>MD</p>
+        </div>
+        <h2 className="clients-h2 max-w-[530px]  text[#efefef]">
+          MD is a highly sought-after speaker and trainer, with a rich reservoir
+          of experience and proficiency in leadership development and business
+          growth strategies.
+        </h2>
+      </div>
 
       <div className="flex flex-col justify-center items-center gap-10 pt-10 px-10 ">
         <div className=" flex flex-col gap-5 ">
--- src/Pages/Book.jsx
+++ src/Pages/Book.jsx
@@ -511,9 +511,9 @@ const Book = () => {
     <main id="book" className="w-full">
       <div
         id="book-hero"
-        className="flex flex-col justify-center text-white gap-2 pl-5  md:pl-60 pt-10"
+        className="flex flex-col justify-center text-white gap-2 pl-5  md:pl-52 pt-10"
       >
-        <div className="flex  gap-2 text-4xl text-wrap font-extrabold">
+        <div className="flex  gap-2 text-5xl text-wrap font-extrabold">
           <p className="text-[#F6C228]">BOOKING</p>
           <p>FORM</p>
         </div>
