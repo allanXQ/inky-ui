a7e946fa3f7bcb12262b9514cfe46b6e13f15286
Allan Juma <allancjuma@gmail.com>
2024-08-05 03:13:43 +0300

    new elements

--- src/Pages/Book.jsx
+++ src/Pages/Book.jsx
@@ -509,7 +509,48 @@ const BookForm = () => {
 const Book = () => {
   return (
     <main id="book" className="w-full">
-      <div id="book-hero">{/* <img src="./bookhero.webp" /> */}</div>
+      <div
+        id="book-hero"
+        // className="flex flex-col justify-center text-white gap-2 px-2 md:pl-60 pt-10"
+      >
+        {/* <div className="flex  gap-2 text-xl text-wrap font-extrabold">
+          <p className="text-[#F6C228]">BOOKING</p>
+          <p>FORM</p>
+        </div>
+        <h2 className="clients-h2 max-w-[530px]  text[#efefef]">
+          Complete the form below if you'd like more information about booking
+          MD.
+        </h2>
+        <div className="flex flex-col gap-2">
+          <div className="flex gap-3 items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="#F6C228"
+              class="size-6"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            <p>254798765432</p>
+          </div>
+          <div className="flex gap-3 items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="#F6C228"
+              class="size-6"
+            >
+              <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
+              <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
+            </svg>
+            <p>booking@md.com</p>
+          </div>
+        </div> */}
+      </div>
 
       <div className="flex justify-center items-center pt-10">
         <BookForm />
