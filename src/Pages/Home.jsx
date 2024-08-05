95cae771e6606574e90ac3b15092adc3f376916e
Allan Juma <allancjuma@gmail.com>
2024-08-05 04:06:13 +0300

    hero responsive

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -80,13 +80,13 @@ export default function Home() {
           className="flex flex-col  gap-4 items-center justify-center  min-h-screen "
         >
           <div className="flex items-center justify-center gap-4 w-2/3">
-            {/* <img
+            <img
               id="hero-inky"
               src="./Inky.webp"
               width={349}
               height={700}
-              className="absolute left-44 top-20"
-            /> */}
+              className="absolute left-10 md:left-48 top-32 md:top-20"
+            />
             <div
               id="hero-text"
               className="flex flex-col items-center justify-center z-20 mt-20 sm:mt-56 gap-6"
