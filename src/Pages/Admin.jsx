1cddcdbdbbd376b3bdd6998cb1406ca3046736b4
Allan Juma <allancjuma@gmail.com>
2024-08-15 13:53:15 +0300

    add new feature

--- src/Pages/Admin.jsx
+++ src/Pages/Admin.jsx
@@ -9,6 +9,8 @@ const AdminDashboard = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
   const [showModal, setShowModal] = useState(false);
+  const [modalType, setModalType] = useState(""); // To determine which modal to show
+  const [newItem, setNewItem] = useState(""); // To handle the input for new item
 
   const fetchAllData = async () => {
     try {
@@ -49,48 +51,84 @@ const AdminDashboard = () => {
     }
   };
 
-  const handleCloseModal = () => {
-    setShowModal(false);
+  const handleAddNew = async (type) => {
+    if (!newItem) {
+      setError("Input cannot be empty");
+      setShowModal(true);
+      return;
+    }
+    console.log(type, newItem);
+    try {
+      await axios.post(`http://localhost:8000/api/${type}`, { name: newItem });
+      setNewItem(""); // Clear the input after posting
+      fetchAllData(); // Refresh data after adding new item
+      setShowModal(false); // Close modal after adding
+    } catch (err) {
+      setError("Failed to add new item");
+      setShowModal(true);
+    }
   };
 
+  const openAddModal = (type) => {
+    setModalType(type);
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setNewItem("");
+  };
+
+  const renderAddButton = (type) => (
+    <button
+      onClick={() => openAddModal(type)}
+      className="mt-2 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+    >
+      Add New
+    </button>
+  );
+
   const renderTable = (data, type) => (
-    <table className="min-w-full leading-normal">
-      <thead>
-        <tr>
-          {Object.keys(data[0]).map((key) => (
-            <th
-              key={key}
-              className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
-            >
-              {key}
-            </th>
-          ))}
-          <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100"></th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((item) => (
-          <tr key={item.id}>
-            {Object.values(item).map((val, index) => (
-              <td
-                key={index}
-                className="px-5 py-5 border-b border-gray-200  text-sm"
+    <div>
+      <table className="min-w-full leading-normal">
+        <thead>
+          <tr>
+            {Object.keys(data[0]).map((key) => (
+              <th
+                key={key}
+                className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
               >
-                {val}
-              </td>
+                {key}
+              </th>
             ))}
-            <td className="px-5 py-5 border-b border-gray-200  text-sm">
-              <button
-                onClick={() => handleDelete(item.id, type)}
-                className="text-red-500 hover:text-red-700"
-              >
-                Delete
-              </button>
-            </td>
+            <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100"></th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data.map((item) => (
+            <tr key={item.id}>
+              {Object.values(item).map((val, index) => (
+                <td
+                  key={index}
+                  className="px-5 py-5 border-b border-gray-200 text-sm"
+                >
+                  {val}
+                </td>
+              ))}
+              <td className="px-5 py-5 border-b border-gray-200 text-sm">
+                <button
+                  onClick={() => handleDelete(item.id, type)}
+                  className="text-red-500 hover:text-red-700"
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {renderAddButton(type)}
+    </div>
   );
 
   return (
@@ -98,58 +136,68 @@ const AdminDashboard = () => {
       <div className="container sm:px-8 text-white mt-24">
         <div className="py-8">
           {loading && <p>Loading...</p>}
-          <div className="overflow-x-scroll">
-            <h2 className="text-2xl font-semibold leading-tight">Bookings</h2>
-            {bookings.length > 0 ? (
-              renderTable(bookings, "bookings")
-            ) : (
-              <p>No bookings available</p>
-            )}
-          </div>
-          <div className="mt-8">
-            <h2 className="text-2xl font-semibold leading-tight">
-              Booking Types
-            </h2>
-            {bookingTypes.length > 0 ? (
-              renderTable(bookingTypes, "booking-types")
-            ) : (
-              <p>No booking types available</p>
-            )}
-          </div>
-          <div className="mt-8">
-            <h2 className="text-2xl font-semibold leading-tight">
-              Organization Types
-            </h2>
-            {organizationTypes.length > 0 ? (
-              renderTable(organizationTypes, "organization-types")
-            ) : (
-              <p>No organization types available</p>
-            )}
-          </div>
-
-          <div className="mt-8">
-            <h2 className="text-2xl font-semibold leading-tight">
-              Signature Messages
-            </h2>
-            {signatureMessages.length > 0 ? (
-              renderTable(signatureMessages, "signature-message-topics")
-            ) : (
-              <p>No signature messages available</p>
-            )}
-          </div>
+          {bookings.length > 0 && (
+            <div>
+              <h2 className="text-2xl font-semibold leading-tight">Bookings</h2>
+              {renderTable(bookings, "bookings")}
+            </div>
+          )}
+          {bookingTypes.length > 0 && (
+            <div className="mt-8">
+              <h2 className="text-2xl font-semibold leading-tight">
+                Booking Types
+              </h2>
+              {renderTable(bookingTypes, "booking-types")}
+            </div>
+          )}
+          {organizationTypes.length > 0 && (
+            <div className="mt-8">
+              <h2 className="text-2xl font-semibold leading-tight">
+                Organization Types
+              </h2>
+              {renderTable(organizationTypes, "organization-types")}
+            </div>
+          )}
+          {signatureMessages.length > 0 && (
+            <div className="mt-8">
+              <h2 className="text-2xl font-semibold leading-tight">
+                Signature Messages
+              </h2>
+              {renderTable(signatureMessages, "signature-message-topics")}
+            </div>
+          )}
         </div>
       </div>
       {showModal && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 text-white flex justify-center items-center">
-          <div className=" p-5 rounded-lg">
-            <h2 className="text-lg font-bold">Error</h2>
-            <p className="mb-4">{error}</p>
-            <button
-              onClick={handleCloseModal}
-              className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-700"
-            >
-              Close
-            </button>
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+          <div className="bg-black p-5 rounded-lg text-white">
+            <h2 className="text-lg font-bold">
+              Add New{" "}
+              {modalType
+                .replace("-", " ")
+                .replace(/\b\w/g, (l) => l.toUpperCase())}
+            </h2>
+            <input
+              type="text"
+              value={newItem}
+              onChange={(e) => setNewItem(e.target.value)}
+              className="border border-gray-300 p-2 w-full bg-black text-white"
+              placeholder="Enter name"
+            />
+            <div className="mt-4 flex justify-between">
+              <button
+                onClick={() => handleAddNew(modalType)}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              >
+                Save
+              </button>
+              <button
+                onClick={handleCloseModal}
+                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+              >
+                Cancel
+              </button>
+            </div>
           </div>
         </div>
       )}
