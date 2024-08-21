74d1c4376bca657e64594a7c4240a8b3b275bedf
Allan Juma <allancjuma@gmail.com>
2024-08-21 18:28:23 +0300

    update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -385,7 +385,7 @@ export default function Home() {
               src="./bottomquote.png"
               width={70}
               height={30}
-              className="absolute bottom-[-30px] right-[-20px]"
+              className="absolute bottom-[-35px] right-[-20px]"
             />
           </div>
         </div>
