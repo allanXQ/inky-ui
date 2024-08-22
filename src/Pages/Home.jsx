b1d2239bc4163f408b0e1ae8e6747cd38f7531c7
Allan Juma <allancjuma@gmail.com>
2024-08-22 16:13:04 +0300

    update sylte

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -243,37 +243,38 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <img src="./meetmd.png" className="max-w-[500px] " />
+        <img src="./meetmd.png" className="max-w-[350px] md:max-w-[500px]" />
       </div>
       <div
         id="quote"
-        className="flex flex-col items-center justify-center w-full relative px-4 mt-24"
+        className="flex flex-col items-center justify-center w-full relative px-4 mt-36 md:mt-24"
       >
-        <div>
-          <div id="comp-ky2hvz0a" data-motion-enter="done">
-            <div data-testid="svgRoot-comp-ky2hvz0a" className="max-w-36">
-              <svg
-                preserveAspectRatio="xMidYMid meet"
-                data-bbox="0 0 770.35 1260.4"
-                viewBox="0 0 770.35 1260.4"
-                xmlns="http://www.w3.org/2000/svg"
-                data-type="color"
-                role="presentation"
-                aria-hidden="true"
-              >
-                <g>
-                  <path
-                    d="M456 210.92q10.37-54.67 19.16-75C306.82 265.76 140.68 430.42 195.6 659.86Q158.89 602.47 161 498q-35.15 121.74-32.85 301.22C113 754.81 102.81 651.36 111.3 590.77 26.78 692.34-17.87 931.93 80.94 1010.4 70.76 1036.21 27.58 1167.58 0 1259.84c0 0 .48.21 1.4.56l51.27-42c94.09-377.35 281.19-808.2 547.89-1093.68q-304.69 430.2-439.83 874.42c269.69-95.91 209.45-395.57 349.6-523.35-40.48 7-78.95 42.61-102.46 70.28C446.65 453.81 551.81 462.59 592 296.2 615.42 199.05 694.48 80.87 770.35 0Q512.27 62 456 210.92z"
-                    fill="#f6c228"
-                    data-color="1"
-                  ></path>
-                </g>
-              </svg>
-            </div>
+        <div id="comp-ky2hvz0a" data-motion-enter="done" className="relative">
+          <div
+            data-testid="svgRoot-comp-ky2hvz0a"
+            className="max-w-24 md:max-w-36  mt-16 md:mt-0"
+          >
+            <svg
+              preserveAspectRatio="xMidYMid meet"
+              data-bbox="0 0 770.35 1260.4"
+              viewBox="0 0 770.35 1260.4"
+              xmlns="http://www.w3.org/2000/svg"
+              data-type="color"
+              role="presentation"
+              aria-hidden="true"
+            >
+              <g>
+                <path
+                  d="M456 210.92q10.37-54.67 19.16-75C306.82 265.76 140.68 430.42 195.6 659.86Q158.89 602.47 161 498q-35.15 121.74-32.85 301.22C113 754.81 102.81 651.36 111.3 590.77 26.78 692.34-17.87 931.93 80.94 1010.4 70.76 1036.21 27.58 1167.58 0 1259.84c0 0 .48.21 1.4.56l51.27-42c94.09-377.35 281.19-808.2 547.89-1093.68q-304.69 430.2-439.83 874.42c269.69-95.91 209.45-395.57 349.6-523.35-40.48 7-78.95 42.61-102.46 70.28C446.65 453.81 551.81 462.59 592 296.2 615.42 199.05 694.48 80.87 770.35 0Q512.27 62 456 210.92z"
+                  fill="#f6c228"
+                  data-color="1"
+                ></path>
+              </g>
+            </svg>
           </div>
         </div>
         <div className="flex flex-col gap-2 items-center max-w-[45rem] border border-[#636363] rounded-lg py-10 px-12">
-          <p className="text-2xl text-center">
+          <p className="text-xl md:text-2xl text-center">
             The one thing we all have in common is that we will encounter
             adversity, but we have to decide how we respond to it. My arm and my
             hand are paralyzed but my heart isn’t, my mind isn’t, my dedication
@@ -340,9 +341,7 @@ export default function Home() {
           <div className="testimonial-container flex flex-col gap-5 border border-[#636363] rounded-xl lg:max-w-[40vw] h-fit p-10 relative ">
             <img
               src="./topquote.png"
-              width={70}
-              height={30}
-              className="absolute top-[-30px] left-[-30px] animate-slideInLeft-1"
+              className="absolute top-[-40px]  left-[-10px] md:left-[-20px] animate-slideInLeft-1 w-18"
             />
             <p>
               The messages of dealing with adversity, building character and
