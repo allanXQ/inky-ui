1c37831301c636aabb60a4dc0b29988ff8752209
Allan Juma <allancjuma@gmail.com>
2024-08-22 15:58:16 +0300

    update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -127,7 +127,7 @@ export default function Home() {
       </div>
       <div
         id="clients"
-        className="flex flex-col items-center justify-center mt-80 xl:mt-10"
+        className="flex flex-col items-center justify-center mt-10 xl:mt-10"
       >
         <h2 className="clients-h2 max-w-[530px] text-center text[#efefef]">
           SOME OF MD'S CLIENTS INCLUDE
