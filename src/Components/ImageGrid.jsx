52dd516da968406216d88cb2fd3b189384324dbd
Allan Juma <allancjuma@gmail.com>
2024-08-24 14:36:06 +0300

    add icons

--- src/Components/ImageGrid.jsx
+++ src/Components/ImageGrid.jsx
@@ -185,7 +185,53 @@ const ImageGallery = () => {
         </button>
       )}
       {modalOpen && (
-        <div className="modal">
+        <div className="modal ">
+          <div
+            className="flex flex-col gap-1 items-center justify-center w-10 absolute top-10 left-10
+          "
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="white"
+              className="size-4 self-end"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M8.25 3.75H19.5a.75.75 0 0 1 .75.75v11.25a.75.75 0 0 1-1.5 0V6.31L5.03 20.03a.75.75 0 0 1-1.06-1.06L17.69 5.25H8.25a.75.75 0 0 1 0-1.5Z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="white"
+              className="size-4 self-start"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M20.03 3.97a.75.75 0 0 1 0 1.06L6.31 18.75h9.44a.75.75 0 0 1 0 1.5H4.5a.75.75 0 0 1-.75-.75V8.25a.75.75 0 0 1 1.5 0v9.44L18.97 3.97a.75.75 0 0 1 1.06 0Z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </div>
+          <div className="absolute top-10 left-36 z-50 icon-hide flex gap-3">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="white"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+              />
+            </svg>
+            <p className="text-white">{Math.floor(Math.random() * 20) + 1}</p>
+          </div>
           <button onClick={closeModal} className="close-modal">
             ✖
           </button>
