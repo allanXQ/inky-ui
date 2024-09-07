993f84e15adbd2feb8d7137beb584a1243b7df1d
Allan Juma <allancjuma@gmail.com>
2024-09-07 21:16:45 +0300

    feather animation in sx screen

--- src/Components/Home/Quote.jsx
+++ src/Components/Home/Quote.jsx
@@ -40,7 +40,6 @@ const Quote = () => {
         initial="hidden"
         animate={controls}
         variants={svgVariants}
-        // Use the viewport prop to trigger the animation when the SVG comes into view
         whileInView="visible"
         viewport={{ once: true, amount: 0.5 }}
       >
@@ -67,6 +66,31 @@ const Quote = () => {
           </svg>
         </div>
       </motion.div>
+      {/* feather in sm screens */}
+      <div id="comp-ky2hvz0a" className="relative sm:hidden">
+        <div
+          data-testid="svgRoot-comp-ky2hvz0a"
+          className="max-w-24 md:max-w-36  mt-16 md:mt-0"
+        >
+          <svg
+            preserveAspectRatio="xMidYMid meet"
+            data-bbox="0 0 770.35 1260.4"
+            viewBox="0 0 770.35 1260.4"
+            xmlns="http://www.w3.org/2000/svg"
+            data-type="color"
+            role="presentation"
+            aria-hidden="true"
+          >
+            <g>
+              <path
+                d="M456 210.92q10.37-54.67 19.16-75C306.82 265.76 140.68 430.42 195.6 659.86Q158.89 602.47 161 498q-35.15 121.74-32.85 301.22C113 754.81 102.81 651.36 111.3 590.77 26.78 692.34-17.87 931.93 80.94 1010.4 70.76 1036.21 27.58 1167.58 0 1259.84c0 0 .48.21 1.4.56l51.27-42c94.09-377.35 281.19-808.2 547.89-1093.68q-304.69 430.2-439.83 874.42c269.69-95.91 209.45-395.57 349.6-523.35-40.48 7-78.95 42.61-102.46 70.28C446.65 453.81 551.81 462.59 592 296.2 615.42 199.05 694.48 80.87 770.35 0Q512.27 62 456 210.92z"
+                fill="#f6c228"
+                data-color="1"
+              ></path>
+            </g>
+          </svg>
+        </div>
+      </div>
       <motion.div
         initial="hidden"
         animate={textControls}
