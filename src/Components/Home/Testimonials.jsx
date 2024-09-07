b804d4d1f404f07b38faaf7e25fd4fd60686a8cd
Allan Juma <allancjuma@gmail.com>
2024-09-07 21:05:25 +0300

    add dots

--- src/Components/Home/Testimonials.jsx
+++ src/Components/Home/Testimonials.jsx
@@ -32,6 +32,10 @@ const Testimonials = ({ openTestimonialModal, testimonialContent }) => {
     },
   };
 
+  const handleDotClick = (index) => {
+    setCurrent(index);
+  };
+
   return (
     <div id="testimonials" className="w-full overflow-hidden">
       <div className="flex flex-col gap-20 sm:flex-row sm:gap-0 md:gap-24 mt-4 items-center justify-center px-2 sm:px-0 w-full">
@@ -53,70 +57,84 @@ const Testimonials = ({ openTestimonialModal, testimonialContent }) => {
             </div>
           </div>
         </div>
-        <div className="testimonial-container flex flex-col gap-5 border border-[#636363] rounded-xl sm:max-w-[60vw] md:max-w-[45vw] h-fit p-10 relative max-h-[450px]">
-          <img
-            src="./topquote.png"
-            className="absolute top-[-40px] left-[-5px] md:left-[0px] animate-slideInLeft-1 w-18"
-          />
-          <AnimatePresence mode="wait" onExitComplete={() => null}>
-            <motion.div
-              key={current}
-              initial="enter"
-              animate="center"
-              exit="exit"
-              variants={variants}
-              className="flex flex-col items-center overflow-hidden"
-            >
-              <div className="max-h-[245px] overflow-y-hidden">
-                <p className="big-shoulders-regular text-ellipsis max-h-[245px] overflow-y-hidden">
-                  {testimonialContent[current]?.text}
-                </p>
-              </div>
-              <div className="flex flex-col items-end gap-2">
-                <p className="font-bold text-2xl">
-                  - {testimonialContent[current]?.name}
-                </p>
-                <p className="clients-h2 uppercase">
-                  {testimonialContent[current]?.position}
-                </p>
-                <div className="flex items-center border border-white rounded-lg w-56">
-                  <button
-                    id="about-btn"
-                    className="py-2 px-4"
-                    onClick={() => openTestimonialModal(current)}
-                  >
-                    <p className="text-md">Read Full Testimonial</p>
-                  </button>
-                  <div
-                    style={{
-                      borderRight: "1px solid white",
-                      height: "2.5rem",
-                    }}
-                  ></div>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="size-8 pl-3"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
-                    />
-                  </svg>
+        <div>
+          <div className="testimonial-container flex flex-col gap-5 border border-[#636363] rounded-xl sm:max-w-[60vw] md:max-w-[45vw] h-fit p-10 relative max-h-[450px]">
+            <img
+              src="./topquote.png"
+              className="absolute top-[-40px] left-[-5px] md:left-[0px] animate-slideInLeft-1 w-18"
+            />
+            <AnimatePresence mode="wait" onExitComplete={() => null}>
+              <motion.div
+                key={current}
+                initial="enter"
+                animate="center"
+                exit="exit"
+                variants={variants}
+                className="flex flex-col items-center overflow-hidden"
+              >
+                <div className="max-h-[245px] overflow-y-hidden">
+                  <p className="big-shoulders-regular text-ellipsis max-h-[245px] overflow-y-hidden">
+                    {testimonialContent[current]?.text}
+                  </p>
                 </div>
-              </div>
-            </motion.div>
-          </AnimatePresence>
-          <img
-            src="./bottomquote.png"
-            width={70}
-            height={30}
-            className="absolute bottom-[-35px] right-[5px] md:right-[-5px]"
-          />
+                <div className="flex flex-col items-end gap-2">
+                  <p className="font-bold text-2xl">
+                    - {testimonialContent[current]?.name}
+                  </p>
+                  <p className="clients-h2 uppercase">
+                    {testimonialContent[current]?.position}
+                  </p>
+                  <div className="flex items-center border border-white rounded-lg w-56">
+                    <button
+                      id="about-btn"
+                      className="py-2 px-4"
+                      onClick={() => openTestimonialModal(current)}
+                    >
+                      <p className="text-md">Read Full Testimonial</p>
+                    </button>
+                    <div
+                      style={{
+                        borderRight: "1px solid white",
+                        height: "2.5rem",
+                      }}
+                    ></div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-8 pl-3"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+                      />
+                    </svg>
+                  </div>
+                </div>
+              </motion.div>
+            </AnimatePresence>
+            <img
+              src="./bottomquote.png"
+              width={70}
+              height={30}
+              className="absolute bottom-[-35px] right-[5px] md:right-[-5px]"
+            />
+          </div>
+          <div className="flex justify-center mt-5">
+            {testimonialContent.map((_, idx) => (
+              <span
+                key={idx}
+                className={`dot ${current === idx ? "active" : ""}`}
+                onClick={() => handleDotClick(idx)}
+                style={{ cursor: "pointer", padding: "5px" }}
+              >
+                ●
+              </span>
+            ))}
+          </div>
         </div>
       </div>
     </div>
